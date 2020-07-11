d7977d561405a24a54e9fceda05a8cb8bf30271a
Cheryl Jensen <cherylbjensen@gmail.com>
2020-07-11 05:18:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ phrase == "i love you grandma"
 if phrase.downcase
   puts "HUH?! SPEAK UP, SONNY!"
 elsif phrase.capitalize
-  puts "NO, NOT SINCE 1938!"
+  puts "WHAT? NO, NOT SINCE 1938!"
 else phrase.upcase
   puts "I LOVE YOU TOO PUMPKIN!"
 end 
