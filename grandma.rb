379b262ae094b0cad9b652107e932080b7b15582
Cheryl Jensen <cherylbjensen@gmail.com>
2020-07-11 05:33:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,6 +8,8 @@ elsif phrase == "Hi!"
   return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == "WHAT DID YOU EAT TODAY?"
   return "NO, NOT SINCE 1938!"
+elsif phrase == "WHAT?"
+  return "NO, NOT SINCE 1938!"
 else phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 end 
