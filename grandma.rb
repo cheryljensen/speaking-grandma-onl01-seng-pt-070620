80d0c202dffa4510202aafb4a1886f466913a605
Cheryl Jensen <cherylbjensen@gmail.com>
2020-07-11 05:17:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,12 @@
 def speak_to_grandma(phrase)
 
-if phrase == "unless you are shouting".downcase
+phrase == "i love you grandma"
+
+if phrase.downcase
   puts "HUH?! SPEAK UP, SONNY!"
-elsif phrase == "when she can hear you".capitalize
+elsif phrase.capitalize
   puts "NO, NOT SINCE 1938!"
-else phrase == "I LOVE YOU GRANDMA!".upcase
+else phrase.upcase
   puts "I LOVE YOU TOO PUMPKIN!"
 end 
 
