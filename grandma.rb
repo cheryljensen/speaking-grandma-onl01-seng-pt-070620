71243716d99fa5c75768b8fb3c44cfca5a83945a
Cheryl Jensen <cherylbjensen@gmail.com>
2020-07-11 05:28:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ phrase == "i love you grandma".upcase
 
 if phrase == "Hi Nana, how are you?"
   return "HUH?! SPEAK UP, SONNY!"
-elsif phrase == "Nana, are you thirsty?"
+elsif phrase == "WHAT DID YOU EAT TODAY?"
   return "WHAT? NO, NOT SINCE 1938!"
 else phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
